2404cf56ac8e8427c9f2b83c2f846b8821fd14f3
451suman <sumanmushyakhwo@gmail.com>
2025-02-04 11:40:30 +0545

    a

--- git_push.bat
+++ git_push.bat
@@ -1,30 +1,28 @@
 @echo off
 
-:: Check if commit message is provided
-if "%~1" == "" (
-    echo Error: No commit message provided.
+:: Check if commit message and branch name are provided
+if "%~1"=="" (
+    echo Error: Commit message is required.
     exit /b 1
 )
 
-:: Display commit message
-echo Commit message is: "%~1"
-set commit_message=%~1
-
-:: Check if branch name is provided
-if "%~2" == "" (
-    echo Error: No branch provided.
+if "%~2"=="" (
+    echo Error: Branch name is required.
     exit /b 1
 )
 
-:: Display branch name
-echo Branch name is: "%~2"
-set branch_name=%~2
+:: Assign parameters to variables
+set COMMIT_MESSAGE=%~1
+set BRANCH_NAME=%~2
 
-
+:: Add all changes to git
 git add .
 
-git commit -m "%commit_message%"
-git push ogigin %branch_name%
+:: Commit with the provided message
+git commit -m "%COMMIT_MESSAGE%"
+
+:: Push changes to the provided branch
+git push origin %BRANCH_NAME%
 
 echo Changes have been added, committed, and pushed to branch %BRANCH_NAME% successfully.
 pause
